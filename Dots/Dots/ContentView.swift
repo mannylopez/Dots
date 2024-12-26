52b3cc3adc0f5b1fa5ee22610ff279562c5ba2ac
mannylopez <manuel.est.lopez@gmail.com>
2024-12-25 21:46:06 -0800

    First light

---
+++ Dots/Dots/ContentView.swift
@@ -0,0 +1,19 @@
+// Created by manny_lopez on 12/25/24.
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ Dots/Dots/DotsApp.swift
@@ -0,0 +1,12 @@
+// Created by manny_lopez on 12/25/24.
+
+import SwiftUI
+
+@main
+struct DotsApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
